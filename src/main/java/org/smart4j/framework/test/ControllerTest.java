e6b64f5bcd5887c8ca392fb0479de789f9588374
MrTaozui <965369592@qq.com>
2018-08-09 15:00:42 +0800

    测试service注解Inject注解

--- src/main/java/org/smart4j/framework/test/ControllerTest.java
+++ src/main/java/org/smart4j/framework/test/ControllerTest.java
@@ -2,13 +2,17 @@ package org.smart4j.framework.test;
 
 import org.smart4j.framework.annotation.Action;
 import org.smart4j.framework.annotation.Controller;
+import org.smart4j.framework.annotation.Inject;
 import org.smart4j.framework.bean.Data;
 import org.smart4j.framework.bean.Param;
 
 @Controller
 public class ControllerTest {
+	@Inject
+	ServiceTest serviceDemo;
 	@Action("get:/la")
 	public Data fun(Param param){
+		//serviceDemo.print();
 		return new Data("lalallala");
 	}
 
---
+++ src/main/java/org/smart4j/framework/test/ServiceTest.java
@@ -0,0 +1,11 @@
+package org.smart4j.framework.test;
+
+import org.smart4j.framework.annotation.Service;
+
+@Service
+public class ServiceTest {
+	public void print(){
+		System.out.println("Hello world!");
+	}
+
+}
